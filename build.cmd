4326329ddf5be8fb7aa48d341fca1d6671e313a3
Ilya Seleznev <itsuart@gmail.com>
2012-07-05 23:38:42 +0700

    gave proper name

--- build.cmd
+++ build.cmd
@@ -3,4 +3,4 @@ rm main.obj main.exe
 REM compile
 cc --std=c99 -c main.c -o main.obj
 REM link
-ld -s --subsystem console --kill-at -e _Main -L"c:\MinGW\lib"  main.obj -o main.exe -lkernel32
+ld -s --subsystem console --kill-at -e _Main -L"c:\MinGW\lib"  main.obj -o tmpsrv.exe -lkernel32
