465eea61e68539a3ecab7eb741faf74ebb564685
Ilya Seleznev <itsuart@gmail.com>
2012-07-05 23:37:50 +0700

    enough for today

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 REM clean
 rm main.obj main.exe
 REM compile
-cc -c main.c -o main.obj
+cc --std=c99 -c main.c -o main.obj
 REM link
 ld -s --subsystem console --kill-at -e _Main -L"c:\MinGW\lib"  main.obj -o main.exe -lkernel32
--- main.c
+++ main.c
@@ -3,8 +3,42 @@
 
 #include <windows.h>
 
-static USHORT* TempDir = L"c:\\tmp";
+static USHORT* TempDir = L"c:\\tmp\\";
+
+static BOOL CreateTmpDir(){
+	BOOL result = CreateDirectoryW(TempDir, NULL);
+	if (!result){
+		//ERROR_ALREADY_EXISTS will be returned if *anything* with such name already exists
+		//in my case directory or file
+		//I can't believe Windows won OS competition.
+		//It's api is plainly was designed by retards. If was designed at all.
+		return ERROR_ALREADY_EXISTS == GetLastError();
+	} else {
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+static BOOL RemoveTmpDir(){
+	// unfortunately, RemoveDirectoryW can't remove non-empty directories :(
+	return DeleteFileW(TempDir);
+	//return FALSE;
+}
+
+static void WriteString(USHORT* string){
+	DWORD dummy;
+	HANDLE stdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	WriteConsoleW(stdout, string, lstrlenW(string), &dummy, NULL);
+}
 
 void Main(){
+	BOOL succeeded = CreateTmpDir();
+	if (succeeded){
+		WriteString(L"Creation succeeded\n");
+		if (RemoveTmpDir()){
+			WriteString(L"Removing succeeded\n");
+		}
+	}
 	ExitProcess(SUCCESS);
 }
